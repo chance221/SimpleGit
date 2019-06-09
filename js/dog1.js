795f198d8a20103155eb827a8162e5ec80acc127
chance221 <39056884+chance221@users.noreply.github.com>
2019-06-09 15:41:54 -0400

    Add files via upload

--- js/dog1.js
+++ js/dog1.js
@@ -1,24 +1,81 @@
 'use strict'
 
 let numOfDogs = document.getElementById('dogNum').value;
-let sumbitBtn = document.getElementById('btn').value;
-let form = document.getElementById('forms').value;
+
+
+let url = `https://dog.ceo/api/breeds/image/random/${numOfDogs}` 
+const dogSpace = document.getElementById('images')
 
 
 function getDogImage(){
     fetch(`https://dog.ceo/api/breeds/image/random/${numOfDogs}`)
-    .then(response=> response.json())
-    .then(responseJson=>console.log(responseJson));
+    .then(function(response) {return response.json()})//turns what is returned from the fetch methd and turns it into a json object
+    .then(function(data){
+                                     
+        let dogPictures = data.message; //get the results passed into the .then function and sets a variable for the json object. It is "resolving it to be a json object"
+        console.log(data.message);
+        return dogPictures.map(function(currentValue,index){
+            console.log(currentValue)
+            let div = createNode('div') //create all the elements we need
+            let img = createNode('img');
+            img.src = currentValue; //add the source of the image to the created node
+            img.alt = `Picture of random dog #${index+1}`
+            img.setAttribute("class", "normImg")
+            img.setAttribute("width", "150")
+            img.setAttribute("height", "150")
+            console.log(img)
+            append(document.getElementById("images"), div);
+            append(div, img);
+            
+        })
+    })
+    .catch(error => alert("something ant right")
+    )
+    
 }
 
+//Create the type of element you need by passing it in as a parmeter)
+
+function createNode(element){
+    return document.createElement(element);
+}; 
+
+//append the specified element to the parent by passing both in as parameters
+function append(parent, el){
+    return parent.appendChild(el)
+}
+
+function updateValue(){
+    numOfDogs = document.getElementById('dogNum').value;
+    if (numOfDogs == undefined){
+        numofDogs = 3
+    }
+    return numOfDogs;
+}
 
 function watchForm(){
-    debugger
-    form.submit(event =>{
-        
+    $('form').submit(event =>{
         event.preventDefault();
-        getDogImage(numOfDogs);
+        getDogImage();
     })
+};
+
+function logImg(responseJson){
+    
+    console.log(responseJson);
+    let jsonArray = responseJson.message
+    
+    // jsonArray.forEach(function(item, index){
+    // htmlArray.push(`<img src= ${item} alt="random dog pcture">`)
+    // });
+    // htmlArray.forEach(writeToDom());
+};
+
+function writeToDom(item){
+    $('.images').append($(
+        alert('this is not working')
+        // `<img src= ${item} alt="random dog pcture">`
+    ))
 }
 
 
